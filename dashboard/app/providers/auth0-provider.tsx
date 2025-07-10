bb3b4fe684a4e32faed72db30337b443a0d6b59e
Alw3ys <alw3ys@gmail.com>
2025-07-10 13:57:03 +0200

    remote config

--- dashboard/app/providers/auth0-provider.tsx
+++ dashboard/app/providers/auth0-provider.tsx
@@ -2,19 +2,24 @@
 
 import { Auth0Provider } from '@auth0/auth0-react';
 import { ReactNode } from 'react';
+import { useConfig } from '@/app/hooks/config';
 
 interface Auth0ProviderWrapperProps {
   children: ReactNode;
 }
 
 export default function Auth0ProviderWrapper({ children }: Auth0ProviderWrapperProps) {
+  const { config, loading } = useConfig();
+  if (loading) {
+    return null;
+  }
   return (
     <Auth0Provider
-      domain="https://teton-ai.eu.auth0.com"
-      clientId="1XPAp9LsuOddURHVDNwiL2H8rhCPhTGE"
+      domain={config!.AUTH0_DOMAIN}
+      clientId={config!.AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: "http://localhost:3000",
-        audience: "https://teton.ai",
+        redirect_uri: config!.AUTH0_REDIRECT_URI,
+        audience: config!.AUTH0_AUDIENCE,
         scope: "openid profile email"
       }}
     >
