6f0205addc49b270978895d8ad572b68a75cce5f
Alw3ys <alw3ys@gmail.com>
2025-09-02 09:57:13 +0200

    feat: ip addreses list

--- api/src/main.rs
+++ api/src/main.rs
@@ -193,7 +193,11 @@ async fn start_main_server(config: &'static Config, authorization: Authorization
         .routes(routes!(
             handlers::distributions::get_distribution_latest_release
         ))
-        .routes(routes!(handlers::ip_address::get_ip_address_info))
+        .routes(routes!(handlers::ip_address::get_ip_addresses,))
+        .routes(routes!(
+            handlers::ip_address::get_ip_address_info,
+            handlers::ip_address::update_ip_address
+        ))
         .routes(routes!(handlers::releases::get_releases))
         .routes(routes!(
             handlers::releases::get_release,
