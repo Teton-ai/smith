fb3df2c1bffaa5c2c919704c012484b28764484b
dduuukk <chrisvsbender@protonmail.com>
2025-11-21 16:58:49 +0100

    feat: add file metadata route

--- api/src/storage.rs
+++ api/src/storage.rs
@@ -65,4 +65,44 @@ impl Storage {
 
         Ok(response)
     }
+    pub async fn head_from_s3(
+        bucket_name: &str,
+        path: Option<&str>,
+        file_name: &str,
+    ) -> anyhow::Result<Response> {
+        let region = Region::from_default_env()?;
+        let credentials = Credentials::default()?;
+        let bucket = Bucket::new(bucket_name, region, credentials)?;
+
+        let object_key = match path {
+            Some(p) => {
+                if !p.is_empty() {
+                    format!("{}/{}", p, file_name)
+                } else {
+                    file_name.to_string()
+                }
+            }
+            None => file_name.to_string(),
+        };
+
+        let (head_object, _code) = bucket.head_object(&object_key).await?;
+
+        // Get the values, handling Options
+        let content_length = head_object
+            .content_length
+            .ok_or_else(|| anyhow::anyhow!("Content-Length missing"))?;
+
+        let etag = head_object
+            .e_tag
+            .ok_or_else(|| anyhow::anyhow!("ETag missing"))?;
+
+        // Create a response with the location header
+        let response = axum::response::Response::builder()
+            .header(axum::http::header::CONTENT_LENGTH, content_length)
+            .header(axum::http::header::ETAG, etag)
+            .body(axum::body::Body::empty())
+            .map_err(anyhow::Error::from)?;
+
+        Ok(response)
+    }
 }
