b9c2a820a23402ad00874796d016f076ff877315
SorenHolstHansen <soren@teton.ai>
2025-12-09 13:03:57 +0100

    chore: better msg

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -86,7 +86,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             info!("Smith installed! Restarting");
         } else {
             error!("Failed to install smith");
-            info!("Output of install command");
+            info!("Output of install command:");
             let _ = std::io::stderr()
                 .write_all(&output.stderr)
                 .inspect_err(|e| error!("Failed to write output of installation to stderr. {e}"));
