3e892d7c4a5ab9895301b09db03cb3e635971b5f
Luis Silva <luis@teton.ai>
2026-01-27 16:43:47 +0100

    chore: sanity check

--- smithd/src/daemon.rs
+++ smithd/src/daemon.rs
@@ -13,6 +13,7 @@ use crate::utils::system::SystemInfo;
 use tracing::info;
 
 pub async fn run() {
+    info!("8================D");
     SystemInfo::new().await.print();
 
     let shutdown = ShutdownHandler::new();
