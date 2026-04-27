d64fc1349c5cdce2328f471f518ed966c3f89eec
Luis Silva <luis@teton.ai>
2026-04-27 12:11:00 +0200

    chore: remove unused

--- cli/src/api.rs
+++ cli/src/api.rs
@@ -587,30 +587,6 @@ impl SmithAPI {
         Ok(command.clone())
     }
 
-    pub async fn send_custom_command(&self, device_id: u64, cmd: String) -> Result<(u64, u64)> {
-        let client = Client::new();
-
-        let custom_command = schema::SafeCommandRequest {
-            id: 0,
-            command: schema::SafeCommandTx::FreeForm { cmd },
-            continue_on_error: false,
-        };
-
-        let resp = client
-            .post(format!("{}/devices/{device_id}/commands", self.domain))
-            .header("Authorization", format!("Bearer {}", &self.bearer_token))
-            .json(&serde_json::json!([custom_command]))
-            .send();
-
-        resp.await?.error_for_status()?;
-
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-
-        let last_command = self.get_last_command(device_id).await?;
-
-        Ok((device_id, last_command.cmd_id as u64))
-    }
-
     pub async fn update_device_labels(
         &self,
         device_id: u64,
@@ -627,30 +603,6 @@ impl SmithAPI {
         resp.await?.error_for_status()?;
 
         Ok(())
-    }
-
-    pub async fn send_restart_command(&self, device_id: u64) -> Result<(u64, u64)> {
-        let client = Client::new();
-
-        let restart_command = schema::SafeCommandRequest {
-            id: 0,
-            command: schema::SafeCommandTx::Restart,
-            continue_on_error: false,
-        };
-
-        let resp = client
-            .post(format!("{}/devices/{device_id}/commands", self.domain))
-            .header("Authorization", format!("Bearer {}", &self.bearer_token))
-            .json(&serde_json::json!([restart_command]))
-            .send();
-
-        resp.await?.error_for_status()?;
-
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-
-        let last_command = self.get_last_command(device_id).await?;
-
-        Ok((device_id, last_command.cmd_id as u64))
     }
 
     pub async fn send_bundle(
